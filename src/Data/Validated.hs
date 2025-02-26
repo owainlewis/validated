4bb98fdb06d6695e1adcb9b9ce5e4e95e762cdb4
Owain Lewis <owain@owainlewis.com>
2025-02-26 10:53:24 +0000

    Update Validated.hs

--- src/Data/Validated.hs
+++ src/Data/Validated.hs
@@ -1,3 +1,8 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DeriveFoldable #-}
+{-# LANGUAGE DeriveTraversable #-}
+
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Data.Validated
@@ -7,13 +12,44 @@
 -- Maintainer  :  Owain Lewis <owain@owainlewis.com>
 -- Stability   :  provisional
 --
--- This module provides a Validated data type
+-- This module provides a Validated data type for handling validation results
+-- with accumulated errors. Similar to Either but with different semantics
+-- for Applicative instance.
+--
+-- Example usage:
+--
+-- @
+-- validateAge :: Int -> Validated String Int
+-- validateAge n 
+--   | n >= 0 && n <= 120 = Valid n
+--   | otherwise = Invalid "Age must be between 0 and 120"
+--
+-- validateName :: String -> Validated String String
+-- validateName name
+--   | not (null name) = Valid name
+--   | otherwise = Invalid "Name cannot be empty"
+--
+-- data Person = Person String Int
+--
+-- validatePerson :: String -> Int -> Validated String Person
+-- validatePerson name age = 
+--   Person <$> validateName name <*> validateAge age
+-- @
+--
 -----------------------------------------------------------------------------
 
 module Data.Validated
-  ( Validated
+  ( -- * Type
+    Validated(..)
+  , Validation(..)
+    -- * Functions
   , isValid
   , isInvalid
+  , validated
+  , fromValid
+  , fromInvalid
+    -- * Transformer
+  , ValidatedT(..)
   ) where
 
 import           Control.Applicative
@@ -34,15 +70,29 @@ instance Validation Validated where
   invalid = Invalid
   valid = Valid
 
-isValid (Invalid _) = True
-isValid (Valid _) = False
+-- | Check if a Validated value is valid
+isValid :: Validated a b -> Bool
+isValid (Valid _) = True
+isValid (Invalid _) = False
 
-isInvalid (Invalid _) = True
-isInvalid (Valid _) = False
+-- | Check if a Validated value is invalid
+isInvalid :: Validated a b -> Bool
+isInvalid = not . isValid
 
+-- | Pattern match on Validated and apply appropriate function
 validated :: (a -> c) -> (b -> c) -> Validated a b -> c
-validated f _ (Invalid x)  =  f x
-validated _ g (Valid y)    =  g y
+validated f _ (Invalid x) = f x
+validated _ g (Valid y) = g y
+
+-- | Extract the Valid value, or error if Invalid
+fromValid :: Validated a b -> b
+fromValid (Valid x) = x
+fromValid (Invalid _) = error "fromValid: Invalid value"
+
+-- | Extract the Invalid value, or error if Valid
+fromInvalid :: Validated a b -> a
+fromInvalid (Invalid x) = x
+fromInvalid (Valid _) = error "fromInvalid: Valid value"
 
 instance Functor (Validated a) where
     fmap _ (Invalid e) =
@@ -56,10 +106,14 @@ instance Foldable (Validated e) where
 
 instance Monoid e => Monoid (Validated e a) where
     Invalid e1 `mappend` Invalid e2 = Invalid (e1 <> e2)
-    Invalid _ `mappend` Valid a2  = Valid a2
-    Valid a1  `mappend` Invalid _ = Valid a1
-    Valid a1 `mappend` Valid _ = Valid a1
-    mempty = Invalid mempty
+    Invalid e `mappend` Valid _  = Invalid e
+    Valid _ `mappend` Invalid e = Invalid e
+    Valid a `mappend` Valid _ = Valid a
+    mempty = Valid mempty
+
+instance Traversable (Validated e) where
+    traverse _ (Invalid e) = pure (Invalid e)
+    traverse f (Valid a) = Valid <$> f a
 
 instance Bifunctor Validated where
     bimap f _ (Invalid e) = Invalid (f e)
@@ -78,5 +132,17 @@ instance Monad (Validated a) where
     Valid a >>= f = f a
 
 -- Transformer instances
---
 newtype ValidatedT e m a = ValidatedT { runValidatedT :: m (Validated e a) }
+
+instance (Functor m) => Functor (ValidatedT e m) where
+    fmap f = ValidatedT . fmap (fmap f) . runValidatedT
+
+instance (Applicative m) => Applicative (ValidatedT e m) where
+    pure = ValidatedT . pure . pure
+    ValidatedT f <*> ValidatedT v = ValidatedT $ liftA2 (<*>) f v
+
+instance (Monad m) => Monad (ValidatedT e m) where
+    return = pure
+    ValidatedT m >>= f = ValidatedT $ m >>= \case
+        Invalid e -> return (Invalid e)
+        Valid x -> runValidatedT (f x)
